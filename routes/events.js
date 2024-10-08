67a4ae1b2db9e0620ea813397b1bc9c0c8f93f2c
Cristhian Torres <cris_itg@yahoo.es>
2024-10-08 15:54:03 -0600

    Firt commit

---
+++ routes/events.js
@@ -0,0 +1,38 @@
+// Rutas de Eventos /events 
+// host + /api/events
+
+const { Router } = require('express');
+const { check } = require('express-validator');
+const { validarCampos } = require('../middlewares/validar-campos');
+const { validarJWT } = require('../middlewares/validar-jwt');
+const { getEventos, crearEvento, actualizarEvento, eliminarEvento } = require('../controllers/events');
+const { isDate } = require('../helpers/isDate');
+
+const router = Router();
+
+//Todas las rutas tienen que pasar por la validaciòn del JWT
+router.use(validarJWT);
+
+router.get('/', getEventos);
+
+router.post(
+    '/',
+    [
+        check('title', 'El titulo es obligatorio').not().isEmpty(),
+        check('start', 'Fecha de inicio es obligatoria').custom(isDate),
+        check('end', 'Fecha de finalización es obligatoria').custom(isDate),
+        validarCampos
+    ],
+    crearEvento);
+router.put(
+    '/:id',
+    [
+        check('title', 'El titulo es obligatorio').not().isEmpty(),
+        check('start', 'Fecha de inicio es obligatoria').custom(isDate),
+        check('end', 'Fecha de finalización es obligatoria').custom(isDate),
+        validarCampos
+    ],
+    actualizarEvento);
+router.delete('/:id', eliminarEvento);
+
+module.exports = router;
